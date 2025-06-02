bc4c8588e59a45bc841fabe3ac13da295de37ffc
Bambang Adi Prakoso <bambang.prakoso@agriaku.com>
2025-06-02 14:40:29 +0700

    Ignore some configuration files for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,11 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import { globalIgnores } from 'eslint/config'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+
+  // Ignore configuration files
+  globalIgnores(['eslint.config.js', 'prettier.config.js', 'vite.config.js']),
+]
